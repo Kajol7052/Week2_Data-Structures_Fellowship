ebe98ad2279c2c52094429d9b84f622429be9fd6
Kajol7052 <kajol7052@gmail.com>
2021-01-04 22:44:44 +0530

    Added: Program to empty a variable without destroying it

---
+++ Basic_Python/empty_variable.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-01-04 19:05:21
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-01-04 21:37:13
+# @Title: Program to empty a variable without destroying it
+
+
+n = 20
+d = {"x":200}
+l = [1,3,5]
+t = (5,7,8)
+print(type(n)())
+print(type(d)())
+print(type(l)())
+print(type(t)()) 
