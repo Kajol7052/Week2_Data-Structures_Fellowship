dd879e54e553db399f72bdeda70fd12bb36c18a7
Kajol7052 <kajol7052@gmail.com>
2021-01-04 21:52:55 +0530

    Added: Program to accept username and print the reverse order

---
+++ Basic_Python/reverse_username.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-01-04 19:08:13
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-01-04 19:45:29
+# @Title: Accepts user's first and last name and print them in reverse order
+
+# function to print the name in reverse order
+def user_name():    
+    first_name = input('Please enter first name: ')    
+    last_name = input('Please enter last name: ')    
+    print("Hi!" ,last_name,first_name)
+
+user_name()
