3f2459043234abe97df681af9717e0f4e00e2ef5
Kajol7052 <kajol7052@gmail.com>
2021-01-04 22:35:19 +0530

    Added: Program to get the system time

---
+++ Basic_Python/system_time.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-01-04 19:09:45
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-01-04 21:04:54
+# @Title: Program to get the system time
+
+import time
+print()
+print(time.ctime()) #The method ctime() converts a time expressed in seconds since the epoch to a string representing local time.
+print()
