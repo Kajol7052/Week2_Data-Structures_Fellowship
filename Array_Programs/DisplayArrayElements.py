9368efe2c1620de0e4b62a05a3c667055b278f86
Kajol7052 <kajol7052@gmail.com>
2021-01-05 23:39:41 +0530

    Added: Program to create an array and display an array items

---
+++ Array_Programs/DisplayArrayElements.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-01-04 23:03:24
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-01-05 10:08:48
+
+size = 5
+arr = []
+for x in range(size):
+    element=(int(input("Enter the array elements: ")))
+    arr.append(element)
+for x in range(size):
+    print(arr[x])
