1c48048334f222674b21ac3c5606dd7b47e1ac0e
Kajol7052 <kajol7052@gmail.com>
2021-01-10 22:50:42 +0530

    Added: Program to append a list to the second list

---
+++ List_Programs/AppendList.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-01-06 19:56:38
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-01-08 00:57:29
+# @Title: Program to append a list to the second list 
+
+
+if __name__ == "__main__":
+    list_a = [1, 2, 3, 4, 5, 6, 'yellow']
+    list_b = [1, 2, 3, 4, 5]
+    combi_list = list_a + list_b
+    print(combi_list)
